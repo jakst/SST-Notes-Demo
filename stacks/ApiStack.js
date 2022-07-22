e8a2f59c09930abf0111fb2ddc81dfebba4fe70f
Jakob Norlin <jakob.stahl91@gmail.com>
2022-07-22 17:29:10 +0200

    Add billing endpoint

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -22,6 +22,7 @@ export function ApiStack({ stack }) {
       "GET    /notes/{id}": "functions/get.main",
       "PUT    /notes/{id}": "functions/update.main",
       "DELETE /notes/{id}": "functions/delete.main",
+      "POST   /billing": "functions/billing.main",
     },
   });
 
