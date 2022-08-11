7e6a36377ebbbc1f78fd0e42bdf2fcd06babb9b1
Jakob Norlin <jakob.stahl91@gmail.com>
2022-08-11 11:38:33 +0200

    Create note authoring page

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 import Signup from "./containers/Signup";
 
@@ -11,6 +12,7 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
       <Route path="*" element={<NotFound />} />;
     </Routes>
   );
