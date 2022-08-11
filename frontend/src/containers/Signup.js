34086694cda9ddd90cddc1b63acc243efece3ead
Jakob Norlin <jakob.stahl91@gmail.com>
2022-08-11 11:26:15 +0200

    Create new user with Amplify Auth

--- frontend/src/containers/Signup.js
+++ frontend/src/containers/Signup.js
@@ -1,8 +1,10 @@
+import { Auth } from "aws-amplify";
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
 import { useAppContext } from "../lib/contextLib";
+import { onError } from "../lib/errorLib";
 import { useFormFields } from "../lib/hooksLib";
 import "./Signup.css";
 
@@ -15,7 +17,7 @@ export default function Signup() {
   });
   const navigate = useNavigate();
   const [newUser, setNewUser] = useState(null);
-  const { userHasAuthenticated } = useAppContext();
+  const { setUserIsAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
@@ -33,13 +35,40 @@ export default function Signup() {
   async function handleSubmit(event) {
     event.preventDefault();
     setIsLoading(true);
-    setNewUser("test");
-    setIsLoading(false);
+    try {
+      const newUser = await Auth.signUp({
+        username: fields.email,
+        password: fields.password,
+      });
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
 
   async function handleConfirmationSubmit(event) {
+    /*
+      Confirm manually with:
+
+      aws cognito-idp admin-confirm-sign-up \
+        --region COGNITO_REGION \
+        --user-pool-id USER_POOL_ID \
+        --username YOUR_USER_EMAIL
+    */
+
     event.preventDefault();
     setIsLoading(true);
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      await Auth.signIn(fields.email, fields.password);
+      setUserIsAuthenticated(true);
+      navigate("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
 
   function renderConfirmationForm() {
