3ca38fd43fc75f7a3c78ba8da7cb0119e360d9f0
Jakob Norlin <jakob.stahl91@gmail.com>
2022-08-11 11:11:50 +0200

    Add form util hook

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
 import { useAppContext } from "../lib/contextLib";
 import { onError } from "../lib/errorLib";
+import { useFormFields } from "../lib/hooksLib";
 import "./Login.css";
 
 export default function Login() {
@@ -12,11 +13,13 @@ export default function Login() {
   const { setUserIsAuthenticated } = useAppContext();
 
   const [isLoading, setIsLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
@@ -25,7 +28,7 @@ export default function Login() {
     setIsLoading(true);
 
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       setUserIsAuthenticated(true);
       navigate("/");
     } catch (e) {
@@ -43,8 +46,8 @@ export default function Login() {
             autoFocus
             type="email"
             autoComplete="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
@@ -52,8 +55,8 @@ export default function Login() {
           <Form.Control
             type="password"
             autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <LoaderButton
